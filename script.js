9db91d0c500576b222dc046fee271cb486e00298
rafaelaguiar10 <rafaelaguiar10@yahoo.com.br>
2023-12-15 12:30:08 -0300

    projeto módulo 2 -- JS React

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 async function createTodoItem(){
-    let novoItem = {
-        id: items.length + 1,
-        descricao: document.getElementById('novoItem').value,
-        completo: false
-      }
-    
-      let response = await fetch('http://localhost:3000/api/todos', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: novoItem.descricao,
-            owner:'Rafael'
-        })
-    
-      })
-    console.log(await response.json())
-    adicionarItem(novoItem)
+  let novoItem = {
+    id: items.length + 1,
+    descricao: document.getElementById('novoItem').value,
+    completo: false
+  }
+
+  let response = await fetch('http://localhost:3000/api/todos', {
+    method: 'POST',
+    body: JSON.stringify({
+        name: novoItem.descricao,
+        owner:'Rafael'
+    })
+  })
+
+  itemCriado = await response.json()
+
+  adicionarItem(itemCriado)
 }
 
 async function loadTodoItems(){
-    let response = await fetch('http://localhost:3000/api/todos')
-    let todoItems = await response.json()
+  let response = await fetch('http://localhost:3000/api/todos')
+  let todoItems = await response.json()
 
-    todoItems.forEach(todoItem => {
-        let item = {
-            descricao: todoItem.name,
-            completo: todoItem.done
-        }
-
-        adicionarItem(item)
-    })
+  todoItems.forEach(todoItem => {
+    adicionarItem(todoItem)
+  })
 }
 
 async function deleteTodoItems(id){
@@ -37,8 +33,11 @@ async function deleteTodoItems(id){
   })
 }
 
-
-
+async function atualizaTodoItems(id){
+  await fetch(`http://localhost:3000/api/todos/${id}`, {
+    method: "PATCH"
+  })
+}
 
 function marcarComoCompletado(item, completo) {
   item.completo = completo
@@ -47,11 +46,13 @@ function marcarComoCompletado(item, completo) {
 }
 
 function adicionarItem(novoItem) {
+  console.log(novoItem)
+
   let itemsList = document.getElementById('lista')
   let itemElement = document.createElement('li')
 
   let itemText = document.createElement('p')
-  itemText.innerText = novoItem.descricao
+  itemText.innerText = novoItem.name
 
   if (novoItem.completo) {
     itemText.style.textDecoration = 'line-through'
@@ -59,7 +60,7 @@ function adicionarItem(novoItem) {
 
   let completarCheckbox = document.createElement('input')
   completarCheckbox.setAttribute('type', 'checkbox')
-  completarCheckbox.checked = novoItem.completo
+  completarCheckbox.checked = novoItem.done
 
   completarCheckbox.addEventListener('change', () => {
     if (completarCheckbox.checked) {
@@ -71,7 +72,18 @@ function adicionarItem(novoItem) {
     marcarComoCompletado(novoItem, completarCheckbox.checked)
   })
 
+  // Cria um elemento button para remover o item
+  let removerButton = document.createElement('button')
+  removerButton.innerText = 'Apagar'
+
+  removerButton.addEventListener('click', () => {
+    console.log(`Apagando item id: ${novoItem.id}`)
+    deleteTodoItems(novoItem.id)
+    itemElement.remove()
+  })
+
   itemText.appendChild(completarCheckbox)
+  itemText.appendChild(removerButton)
   itemElement.appendChild(itemText)
   itemsList.appendChild(itemElement)
 }
